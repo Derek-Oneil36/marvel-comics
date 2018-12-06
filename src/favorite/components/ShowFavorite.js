5163fda049ed8b03cd82637ab4ccc14ea496e85f
LightCloud36 <dereko.neil@yahoo.com>
2018-12-06 16:05:07 -0500

    outlined a show favorites component

---
+++ src/favorite/components/ShowFavorite.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+
+
+class ShowFavorite extends React.Component {
+  constructor(props){
+    super (props)
+    this.state = {
+      favorite:{
+        name: '',
+        thumbnail: '',
+        description: ''
+      }
+    }
+  }
+
+  componentDidMount(){
+
+    const id = this.props.match.params.id
+    const response =  axios.get(`http://localhost:4741/favorites/${id}`)
+    const favorite = response.data.data.results[0]
+    this.setState({
+      favorite: favorite
+    })
+    console.log(this.state.favorite)
+  }
+
+  render() {
+    if (this.state.favorites.description === '') {
+      return (
+        <React.Fragment>
+
+          <h1>Favorite:</h1>
+          <p>{this.state.favorite.name}</p>
+          <p>{this.state.favorite.thumbnail}</p>
+
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+
+          <h1>Favorite:</h1>
+          <p>{this.state.favorite.name}</p>
+          <p>{this.state.favorite.thumbnail}</p>
+          <p>{this.state.favorite.description}</p>
+
+
+        </React.Fragment>
+      )
+    }
+  };
+
+}
+
+export default ShowFavorite
