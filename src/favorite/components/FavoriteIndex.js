1522e00aae4b0c911fcec3c3e05e25391472d55f
LightCloud36 <dereko.neil@yahoo.com>
2018-12-06 22:48:12 -0500

    renders user favorite characters and delete button

--- src/favorite/components/FavoriteIndex.js
+++ src/favorite/components/FavoriteIndex.js
@@ -1,74 +1,86 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import NewFavorite from '../../favorite/components/NewFavorite'
+import API_URL from '../../apiConfig.js'
+import DeleteFavorite from '../../favorite/components/DeleteFavorite'
 
 const PUBLIC_KEY  = '1e8ae23add929fcff17a2a1be0f7aa53'
-const API_URL = 'https://gateway.marvel.com:443/v1/public/characters'
+const M_API_URL = 'https://gateway.marvel.com:443/v1/public/characters'
 
 class CharacterIndex extends React.Component {
 
   constructor (props) {
     super(props)
-    this.state = { favorites: [], comicIds: []}
+    this.state = { favorites: [], comicIds: [], favIds: [],
+      'user': this.props.user}
 
   }
 
   async componentDidMount() {
     const token = this.props.user.token
     const comics = []
-    axios.get('http://localhost:4741/favorites', { headers: { Authorization: `Bearer ${token}` } })
-
+    axios.get(`${API_URL}/favorites`, { headers: { Authorization: `Bearer ${token}` } })
+      // .then((comicId) => {
+      //   console.log(comicId)
+      // })
       .then((comicIds) => (this.setState({comicIds:comicIds.data.favorites.map(comic => comic.comicId)})))
+      // .then((comicIds) => (this.setState({favIds: comicIds.data.favorites.map(fav => fav._id)})))
 
       .then(() => {
         const getRequests = this.state.comicIds.map(id => {
-          return axios.get(`${API_URL}/${id}?apikey=${PUBLIC_KEY}`)
+          return axios.get(`${M_API_URL}/${id}?apikey=${PUBLIC_KEY}`)
           // comics.push(response)
         })
+        console.log(this.state.favIds)
         Promise.all(getRequests)
           .then((characters) => {
 
             this.setState({favorites: characters})
           })
       })
-      
+
       .catch(console.log)
 
   }
 
+  async handleDelete(event, id) {
+    event.preventDefault()
+
+    const token = this.state.user.token
+
+    const deleteFavorite = (id, token) => {
+      return fetch(`${API_URL}/favorites/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization':`Token token=${token}`
+        }
+      })
+    }
+
+    deleteFavorite(id,token)
+
+  }
+
+
   render() {
-
-    c
-
-    const characterRows = this.state.favorites.map((character, i) => {
+    const favIds = this.state.favIds
+    console.log(this.state.favorites)
+    const characterRows = this.state.favorites.map((character, i, favIds) => {
       const {id, name, thumbnail, description } = character.data.data.results[0]
 
-      if (this.props.user){
-        return (
-          <tr key={i}>
-            <td>
-
-              <h3>{name}</h3>
-              <h4>ID: {id}</h4>
-              <img className="comic-thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`}/>
-              <p>Description: {description}</p>
-              <NewFavorite id={id} component={NewFavorite}/>
-            </td>
-          </tr>
-        )
-      } else {
-        return (
-          <tr key={i}>
-            <td>
-              <h3>{name}</h3>
-              <h4>ID: {id}</h4>
-              <img className="comic-thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`}/>
-              <p>Description: {description}</p>
-            </td>
-          </tr>
-        )
-      }
+      return (
+        <tr key={i}>
+          <td>
+            <Link to={`/favorites/${id}`}><h3>{name}</h3></Link>
+            <h4>ID: {id}</h4>
+            <img className="comic-thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`}/>
+            <p>Description: {description}</p>
+            <button onClick={(event)=> {
+              return this.handleDelete(event, id)
+            }}>Remove</button>
+          </td>
+        </tr>
+      )
     })
 
 
